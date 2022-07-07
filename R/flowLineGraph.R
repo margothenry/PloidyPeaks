ecddc56fa370ae2e74a752b8cf1486718f0335f2
Margot Henry <margothenry@wifi-wpa-cw2-140-193-66-94.wifi.umanitoba.ca>
2022-07-07 09:15:56 -0500

    Added options for user to only use one of the parameters.

--- R/flowLineGraph.R
+++ R/flowLineGraph.R
@@ -16,14 +16,19 @@
 #'
 #' @examples
 #'  flowLineGraph(
-#'   flowControl = "gated_FH-B11",
-#'   flowSamples = c("gated_T1-D08","gated_FH-B08", "gated_T1-D10", "gated_A13-E10"),
+#'   flowControl = "FH-B11",
+#'   flowSamples = c("T1-D08","FH-B08", "T1-D10", "A13-E10"),
 #'   xVariable = "FITC-A",
 #'   flowDir = "FlowData/gated_data"
 #'  )
 
 
-flowLineGraph = function(flowDir = NA, flowControl = NA, flowSamples, xVariable){
+flowLineGraph = function(
+  flowDir = NA,
+  flowControl = NA,
+  flowSamples = NA,
+  xVariable
+){
   ##Removing NOTE 'no visible binding for global variable'
   x<-y<-Data<-NULL
   if(is.na(flowDir)){
@@ -37,33 +42,86 @@ flowLineGraph = function(flowDir = NA, flowControl = NA, flowSamples, xVariable)
   }
   
   ##samples dataset
-  sampleDs <- c()
-  for(k in 1:length(flowSamples)){
-    
-    if(!flowSamples[k] %in% list.files(flowDir)){
-      stop(paste0("The flow frame ",flowSamples[k]," is not in the folder, check on the spelling of flowName and/or make sure you selected the proper folder"))
-    }
-    xpectr::suppress_mw(
-      flowName <- flowCore::read.FCS(
-        paste0(flowDir, "/", flowSamples[k]), transformation=FALSE
+  if(TRUE %in% !is.na(flowSamples)){
+    if(length(flowSamples) > 1){
+      
+      sampleDs <- c()
+      for(k in 1:length(flowSamples)){
+        
+        if(!flowSamples[k] %in% list.files(flowDir)){
+          stop(paste0("The flow frame ",flowSamples[k]," is not in the folder, check on the spelling of flowName and/or make sure you selected the proper folder"))
+        }
+        xpectr::suppress_mw(
+          flowName <- flowCore::read.FCS(
+            paste0(flowDir, "/", flowSamples[k]), transformation=FALSE
+          )
+        )
+        
+        ##Checking to see if the user input the correct X variable
+        if(!xVariable %in% flowName@parameters@data$name){
+          stop("Your X variable is not in the dataset")
+        }
+        
+        ds <- smoothData( flowName, xVariable, 5)
+        ds$Data <- flowSamples[k]
+        sampleDs <- rbind(
+          sampleDs,
+          ds
+        )
+      }
+      
+    }else{
+      xpectr::suppress_mw(
+        flowName <- flowCore::read.FCS(
+          paste0(flowDir, "/", flowSamples), transformation=FALSE
+        ) 
       )
-    )
-    
-    ##Checking to see if the user input the correct X variable
-    if(!xVariable %in% flowName@parameters@data$name){
-      stop("Your X variable is not in the dataset")
+      
+      ##Checking to see if the user input the correct X variable
+      if(!xVariable %in% flowName@parameters@data$name){
+        stop("Your X variable is not in the dataset")
+      }
+      
+      sampleDs <- smoothData(flowName, xVariable, 5)
+      sampleDs$Data <- flowSamples
     }
     
-    ds <- smoothData( flowName, xVariable, 5)
-    ds$Data <- flowName@description[["GUID"]]
-    sampleDs <- rbind(
-      sampleDs,
-      ds
-    )
-  }
-
-
-  if(!is.na(flowControl)){
+    if(!is.na(flowControl)){
+      if(!flowControl %in% list.files(flowDir)){
+        stop(paste0("The flow frame ",flowControl," is not in the folder, check on the spelling of flowName and/or make sure you selected the proper folder"))
+      }
+      ##control dataset
+      xpectr::suppress_mw(
+        flowNameControl <- flowCore::read.FCS(
+          paste0(flowDir, "/", flowControl), transformation=FALSE
+        ) 
+      )
+      
+      ##Checking to see if the user input the correct X variable
+      if(!xVariable %in% flowNameControl@parameters@data$name){
+        stop("Your X variable is not in the dataset")
+      }
+      
+      controlDs <- smoothData(flowNameControl, xVariable, 5)
+      controlDs$Data <- flowControl
+      
+      ##plotting
+      flowPlot <- ggplot() +
+        geom_line(data=sampleDs, aes(x=x, y=y, group=Data, color = Data))+
+        geom_line(data=controlDs, aes(x=x, y=y, group=2), size = 1, color='black')+
+        ylab("Counts")+
+        xlab(xVariable)+
+        theme_bw()
+      
+    }else{
+      ##plotting
+      flowPlot <- ggplot() +
+        geom_line(data=sampleDs, aes(x=x, y=y), size = 1, color='black')+
+        ylab("Counts")+
+        xlab(xVariable)+
+        theme_bw()
+    }
+  }else{
     if(!flowControl %in% list.files(flowDir)){
       stop(paste0("The flow frame ",flowControl," is not in the folder, check on the spelling of flowName and/or make sure you selected the proper folder"))
     }
@@ -80,28 +138,19 @@ flowLineGraph = function(flowDir = NA, flowControl = NA, flowSamples, xVariable)
     }
     
     controlDs <- smoothData(flowNameControl, xVariable, 5)
-    controlDs$Data <- flowNameControl@description[["GUID"]]
+    controlDs$Data <- flowControl
     
     ##plotting
     flowPlot <- ggplot() +
-      geom_line(data=sampleDs, aes(x=x, y=y, group=Data, color = Data))+
       geom_line(data=controlDs, aes(x=x, y=y, group=2), size = 1, color='black')+
-      ylab("Counts")+
-      xlab(xVariable)+
-      theme_bw()
-    
-  }else{
-    ##plotting
-    flowPlot <- ggplot() +
-      geom_line(data=sampleDs, aes(x=x, y=y, group=Data, color = Data))+
       ylab("Counts")+
       xlab(xVariable)+
       theme_bw()
   }
   
+  
+  
   return(flowPlot)
 }
-
-
 
 
